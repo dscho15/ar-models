5a028c85292f7c8f2f70d159fab52545b6131d2b
dscho15 <dscho15@student.sdu.dk>
2024-11-23 07:46:38 +0100

    added vae

--- models/vae.py
+++ models/vae.py
@@ -8,6 +8,7 @@ import numpy as np
 def nonlinearity(x):
     return x * torch.sigmoid(x)
 
+
 def normalize(in_channels, num_groups=32):
     return torch.nn.GroupNorm(
         num_groups=num_groups, num_channels=in_channels, eps=1e-6, affine=True
---
+++ utils.py
@@ -0,0 +1,13 @@
+import torch
+from timm.models.vision_transformer import Block
+
+class MHSA(torch.nn.Module):
+    # x = x + self.drop_path1(self.ls1(self.attn(self.norm1(x))))
+    # x = x + self.drop_path2(self.ls2(self.mlp(self.norm2(x))))
+
+    def __init__(self, dim: int = 128, num_heads: int = 4, mlp_ratio: int = 2):
+        super(MHSA, self).__init__()
+        self.block = Block(dim=dim, num_heads=num_heads, mlp_ratio=mlp_ratio)
+
+    def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
+        return self.block(x)
